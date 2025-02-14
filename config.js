337120f542d8926884118dff49b6e4516f2eab7d
Boss <161413826+itsb0ss@users.noreply.github.com>
2025-02-14 02:43:34 -0800

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,14 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["636615832218239016", ""], 
   mongodbUri : "mongodb+srv://b0ss:MgfaYXNCOzl9p1WV@b0ss-music-v2.gll1e.mongodb.net/?retryWrites=true&w=majority&appName=B0ss-Music-v2",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
-  embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  embedColor: "#89ced3 ",
+  activityName: "B0ss Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/uklnrp",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
