a1638b4ed533299aa9a3086038aa6037316b5ff2
Boss <161413826+itsb0ss@users.noreply.github.com>
2025-02-14 03:54:20 -0800

    Update config.js

--- config.js
+++ config.js
@@ -15,14 +15,28 @@ module.exports = {
   errorLog: "", 
   nodes: [
   {
-      name: "Catfein ID",
-      password: "catfein",
-      host: "lava.catfein.com",
-      port: 4000,
+      name: "ChalresNaig Node",
+      password: "NAIGLAVA-dash.techbyte.host",
+      host: "lavahatry4.techbyte.host",
+      port: 3000,
       secure: false
     },
     { 
-      name: "saher",
+      name: "Public Lavalink v4 NonSSL",
+      password: "https://dsc.gg/ajidevserver",
+      host: "lava-v4.ajieblogs.eu.org",
+      port: 80,
+      secure: false
+      },
+    { 
+      name: "AjieDev - Lavalink [Non SSL]",
+      password: "https://dsc.gg/ajidevserver",
+      host: "lava-v3.ajieblogs.eu.org",
+      port: 80,
+      secure: false
+      },
+    { 
+      name: "INZEWORLD.COM (DE)",
       password: "saher.inzeworld.com",
       host: "lava.inzeworld.com",
       port: 3128,
