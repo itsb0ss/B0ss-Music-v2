50bbe478a518e2409ab674c65e25f62207399bc1
Boss <161413826+itsb0ss@users.noreply.github.com>
2025-02-14 02:57:32 -0800

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ module.exports = {
   mongodbUri : "mongodb+srv://b0ss:MgfaYXNCOzl9p1WV@b0ss-music-v2.gll1e.mongodb.net/?retryWrites=true&w=majority&appName=B0ss-Music-v2",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
-  embedColor: "#89ced3 ",
-  activityName: "B0ss Music", 
+  embedColor: "#89ced3",
+  activityName: "", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/uklnrp",
   embedTimeout: 5, 
