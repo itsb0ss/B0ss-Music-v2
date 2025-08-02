a8b9c8c6498a98677dd9d53df9a327addec624c1
Boss <161413826+itsb0ss@users.noreply.github.com>
2025-08-01 23:50:05 -0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["636615832218239016", ""], 
-  mongodbUri : "mongodb+srv://b0ss:MgfaYXNCOzl9p1WV@b0ss-music-v2.gll1e.mongodb.net/?retryWrites=true&w=majority&appName=B0ss-Music-v2",
+  mongodbUri : "",
   spotifyClientId : "f71a3da30e254962965ca2a89d6f74b9",
   spotifyClientSecret : "199a619d22dd4e55a4a2c1a7a3d70e63",
   setupFilePath: './commands/setup.json',
